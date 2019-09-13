be5f701f77479cd08b6411cc02334992fc732c49
Eric Figueroa <ericleefigueroa@gmail.com>
2019-09-12 23:12:01 -0700

    Full HW did not load

---
+++ assets/javascript/games.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $("#gem1").on("click" , function() {
+
+        alert("You clicked gem 1");
+        
+    
+    
+    })
+    
+    $("#gem2").on("click" , function() {
+    
+        alert("You clicked gem 2");
+        
+    
+    
+    })
+    
+    $("#gem3").on("click" , function() {
+    
+        alert("You clicked gem 2");
+        
+    
+    
+    })
+})
+
